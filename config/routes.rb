8d24f84dd79fe9bb2e414f8167607bc6ca72e312
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2021-10-17 22:07:25 -0300

    creating model and basic validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
 
   get "about", to: "static_pages#about"
   get "contact", to: "static_pages#contact"
+
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
